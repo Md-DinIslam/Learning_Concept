0e3503980be5cf3d1f8dc0b97730b904513675d1
Muhammad Dinislam <108214424+Md-DinIslam@users.noreply.github.com>
2024-11-16 00:30:50 +0600

    Update DSU_Account_Merge.cpp

--- CPP/DSU_Account_Merge.cpp
+++ CPP/DSU_Account_Merge.cpp
@@ -1,66 +1,76 @@
-class solution {
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+
+class DSU {
 private:
-	int Find(int node, vector<int> &par) {
-		if (node == par[node]) {
-			return node;
-		}
-		return par[node] = Find(par[node], par);
-	}
-
-	void Union(int x, int y, vector<int> &sizes, vector<int> &par) {
-		int par_x = Find(x, par);
-		int par_y = Find(y, par);
-		if (par_x == par_y) return;
-		if (sizes[par_x] < sizes[par_y]) {
-			par[par_x] = par_y;
-			sizes[par_y] += sizes[par_x];
-		}
-		else {
-			par[par_y] = par_x;
-			sizes[par_x] += sizes[par_y];
-		}
-	}
-
+    vector<int> sizes, par;
 public:
-	vector<vector<string>> accountsMerge(vector<vector<string>> &accounts) {
-		int n = accounts.size();
-		map<string, int> mailNode;
-		vector<int> sizes(n, 1), par(n);
-		iota(par.begin(), par.end(), 0);
+    DSU(int n) {
+        sizes.resize(n, 1);
+        par.resize(n);
+        iota(par.begin(), par.end(), 0);
+    }
+    int Find(int node) {
+        if(node == par[node]) return node;
+        return par[node] = Find(par[node]);
+    }
+    void Union(int x, int y) {
+        int par_x = Find(x);
+        int par_y = Find(y);
+        if(par_x == par_y) return;
+        if(sizes[par_x] < sizes[par_y]) {
+            par[par_x] = par_y;
+            sizes[par_y] += sizes[par_x];
+        }
+        else {
+            par[par_y] = par_x;
+            sizes[par_x] += sizes[par_y];
+        }
+    }
+};
 
-		for (int i = 0; i < n; ++i) {
-			int m = accounts[i].size();
-			for (int j = 1; j < m; ++j) {
-				string mail = accounts[i][j];
-				if (mailNode.find(mail) == mailNode.end()) {
-					mailNode[mail] = i;
-				}
-				else {
-					Union(i, mailNode[mail], sizes, par);
-				}
-			}
-		}
+class Solution {    
+public:
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        int n = accounts.size();
+        DSU t(n);
 
-		vector<string> mergedMail[n];
-		for (auto &x : mailNode) {
-			string mail = x.first;
-			int node = x.second;
-			int parNode = Find(node, par);
-			mergedMail[parNode].push_back(mail);
-		}
+        map<string, int> mailNode;
+        for(int i = 0;i < n; ++i) {
+            int m = accounts[i].size();
+            for(int j = 1;j < m; ++j) {
+                string mail = accounts[i][j];
+                if(mailNode.count(mail) == 0) {
+                    mailNode[mail] = i;
+                }
+                else {
+                    t.Union(i, mailNode[mail]);
+                }
+            }
+        }
 
-		vector<vector<string>> ans;
-		for (int i = 0; i < n; ++i) {
-			if (mergedMail[i].size()) {
-				vector<string> temp;
-				temp.push_back(accounts[i][0]);
-				for (auto &x : mergedMail[i]) {
-					temp.push_back(x);
-				}
-				ans.push_back(temp);
-			}
-		}
+        vector<string> mergedMail[n];
+        for(auto &pr : mailNode) {
+            string mail = pr.ff;
+            int node = t.Find(pr.ss);
+            mergedMail[node].push_back(mail);
+        }
 
-		return ans;
-	}
+        vector<vector<string>> ans;
+        for(int i = 0;i < n; ++i) {
+            if(mergedMail[i].size()) {
+                vector<string> temp;
+                temp.push_back(accounts[i][0]);
+                sort(all(mergedMail[i]));
+                for(auto &x : mergedMail[i]) {
+                    temp.push_back(x);
+                }
+                // sort(temp.begin() + 1, temp.end());
+                ans.push_back(temp);
+            }
+        }
+
+        return ans;
+    }
 };
